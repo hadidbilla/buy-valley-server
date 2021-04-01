6942e3d7f3bc5455b3e8407c0b4a2b3ed05718f1
hbhridoy449 <50765387+hbhridoy449@users.noreply.github.com>
2021-04-02 04:31:08 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectID = require("mongodb").ObjectID;
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = 4500;
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zta4c.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const items = client.db("buyValley").collection("items");
+  const orders = client.db("buyValley").collection("orders");
+  app.post("/addProducts", (req, res) => {
+    const item = req.body;
+    items.insertOne(item).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+  app.get("/items", (req, res) => {
+    items.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+  app.get("/order", (req, res) => {
+    console.log(req.query.email);
+    orders.find({ email: req.query.email }).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+
+  app.post("/addOrder", (req, res) => {
+    const order = req.body;
+    orders.insertOne(order).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+  app.get("/checkout/:id", (req, res) => {
+    const id = ObjectID(req.params.id);
+    console.log(id);
+    items.find({ _id: id }).toArray((err, documents) => {
+      res.send(documents[0]);
+      console.log(documents);
+    });
+  });
+  app.delete("/deleteItem/:id", (req, res) => {
+    const id = ObjectID(req.params.id);
+    console.log();
+    items.deleteOne({ _id: id }).then((result) => console.log(result));
+  });
+});
+
+app.listen(port);
